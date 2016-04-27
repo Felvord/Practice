15ffe5d15b514a7b92921685a766097150a9fb61
Felvord <tzfelvord@mail.ru>
2016-04-27 10:04:43 +0300

    Add project

---
+++ Practice/Terminal/TerminalClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleClient.ServiceTerminal;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создадим клиент сервиса
+            TerminalClient client = new TerminalClient();
+            Console.Title = "TerminalConsole";
+            Console.WriteLine("Welcome Terminal.Type !help for help.");
+
+            try
+            {
+                // проверка соединения
+                Console.Write("Проверка соединения с сервисом... ");
+                if (!string.Equals(client.testConnection(), "OK", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    throw new Exception("Проверка соединения не удалась");
+                }
+                Console.Write("OK\n");
+                string value = Console.ReadLine();
+                Console.WriteLine(client.getCommand(value));
+
+            }
+            catch (Exception ex)
+            {
+                client.Abort();
+                Console.Write("Error: {0}", ex.Message);
+            }
+
+            client.Close();
+        }
+    }
+}
---
+++ Practice/Terminal/TerminalService/Terminal.svc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TerminalService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class Terminal : ITerminal
+    {
+        public string testConnection()
+        {
+            return "OK";
+        }
+
+        public string getCommand(string str)
+        {
+            switch (str)
+            {
+                case "!help":
+                    {
+                        str ="";
+                        string[] commands = new string[]
+                            {
+                                "1. !help\n",
+                                "2. !showstatus\n",
+                                "3. !store\n",
+                                "4. !free\n",
+                                "5. !exit\n",
+                            };
+                        foreach (var line in commands)
+                            str += line;
+                            return str;
+                    } 
+                     break;
+                case "!showstatus":
+                    {
+                        //
+                        return str;
+                    }
+                    break;
+                case "!store":
+                    {
+                        //
+                        return str;
+                    }
+                    break;
+                case "!free":
+                    {
+                        //
+                        return str;
+                    }
+                    break;
+                case "!exit":
+                    {
+                        //
+                        return str;
+                    }
+                    break;
+                default:
+                    {
+                       Console.Write("             ");
+
+                    }
+                    break;
+            }
+
+            return "ERROR COMMAND";
+        }
+    }
+}
